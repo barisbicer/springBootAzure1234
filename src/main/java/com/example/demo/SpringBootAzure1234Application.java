193453d20dfe4e62f42ab594a866673ce6c9ac92
barisbicer <baris_bicer@hotmail.com>
2023-05-12 14:01:01 +0300

    first

---
+++ src/main/java/com/example/demo/SpringBootAzure1234Application.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+@RestController
+public class SpringBootAzure1234Application {
+
+	@GetMapping(value="/message")
+	public String getMethodName() {
+		return "baris bicer....................................";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzure1234Application.class, args);
+	}
+
+}
