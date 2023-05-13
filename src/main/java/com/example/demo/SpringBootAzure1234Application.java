5938383e5561d739f5e104b76d99c771d74f8afe
barisbicer <baris_bicer@hotmail.com>
2023-05-13 20:02:49 +0300

    update

--- src/main/java/com/example/demo/SpringBootAzure1234Application.java
+++ src/main/java/com/example/demo/SpringBootAzure1234Application.java
@@ -23,7 +23,7 @@ public class SpringBootAzure1234Application {
 	}
 	
 	
-	@GetMapping(value="/heas")
+	@GetMapping(value="/heasmessage")
 	public String getMethodName3() {
 		return "this message is coming from CI/CD process";
 	}
--- src/test/java/com/example/demo/SpringBootAzure1234ApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootAzure1234ApplicationTests.java
@@ -6,8 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootAzure1234ApplicationTests {
 
+
+	
 	@Test
 	void contextLoads() {
+		
+		System.out.println("Test Çalisti süperr");
+	
+		
 	}
 
 }
